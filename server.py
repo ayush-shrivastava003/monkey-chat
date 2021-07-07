7077b2586cb0167212a70812f55bbbcc68d0e962
moistpotato9873 <moistpotato9873@users.noreply.github.com>
2021-07-07 18:22:24 -0400

    run server on pi

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 import select
 
 HEADERSIZE = 10
-IP = "127.0.0.1"
+IP = "0.0.0.0"
 PORT = 1234
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,38 +31,44 @@ def receiveMessage(clientSocket):
 
 
 while True:
-    readSockets, _, exceptionSockets = select.select(socketsList, [], socketsList)
+    try:
+        readSockets, _, exceptionSockets = select.select(socketsList, [], socketsList)
 
-    for notifiedSocket in readSockets:
-        if notifiedSocket == s:
-            clientSocket, clientAddr = s.accept()
+        for notifiedSocket in readSockets:
+            if notifiedSocket == s:
+                clientSocket, clientAddr = s.accept()
 
-            user = receiveMessage(clientSocket)
-            if user is False:
-                continue
+                user = receiveMessage(clientSocket)
+                if user is False:
+                    continue
 
-            socketsList.append(clientSocket)
-            clients[clientSocket] = user
+                socketsList.append(clientSocket)
+                clients[clientSocket] = user
 
-            print("Accepted new connection from {0}:{1}. username: {2}".format(clientAddr[0], clientAddr[1], user["data"].decode("utf-8")))
+                print("Accepted new connection from {0}:{1}. username: {2}".format(clientAddr[0], clientAddr[1], user["data"].decode("utf-8")))
 
-        else:
-            msg = receiveMessage(notifiedSocket)
+            else:
+                msg = receiveMessage(notifiedSocket)
 
-            if msg is False:
-                print("Closed connection from {}".format(clients[notifiedSocket]["data"].decode("utf-8")))
-                socketsList.remove(notifiedSocket)
-                del clients[notifiedSocket]
-                continue
+                if msg is False:
+                    print("Closed connection from {}".format(clients[notifiedSocket]["data"].decode("utf-8")))
+                    socketsList.remove(notifiedSocket)
+                    del clients[notifiedSocket]
+                    continue
 
-            user = clients[notifiedSocket]
-            print("Received message from {}:".format(user["data"].decode("utf-8")))
-            print(msg["data"].decode("utf-8"))
+                user = clients[notifiedSocket]
+                print("Received message from {}:".format(user["data"].decode("utf-8")))
+                print(msg["data"].decode("utf-8"))
 
-            for clientSocket in clients:
-                if clientSocket != notifiedSocket:
-                    clientSocket.send(user["header"] + user["data"] + msg["header"] + msg["data"])
-            
-    for notifiedSocket in exceptionSockets:
-        socketsList.remove(notifiedSocket)
-        del clients[notifiedSocket]
+                for clientSocket in clients:
+                    if clientSocket != notifiedSocket:
+                        clientSocket.send(user["header"] + user["data"] + msg["header"] + msg["data"])
+                
+        for notifiedSocket in exceptionSockets:
+            socketsList.remove(notifiedSocket)
+            del clients[notifiedSocket]
+
+    except KeyboardInterrupt:
+        print("keyboard interrupt")
+        s.shutdown(s.SHUTDOWN_RWDR)
+        s.close()
